bb98e543475487bd51816062bc02cd1d594ba9f8
Han <kein520@yahoo.com>
2019-07-02 09:35:01 -0700

    added data.Dataset class for loading/preproc/lookup

--- data/dataset.py
+++ data/dataset.py
@@ -1,7 +1,6 @@
-"""Dataset class that can be extended by dataset-specific classes."""
+"""Dataset class for CHSI dataset that can be extended by dataset-specific classes."""
 from pathlib import Path
 import pandas as pd
-#import os
 
 
 class Dataset(pd.DataFrame):
@@ -11,8 +10,11 @@ class Dataset(pd.DataFrame):
         pandas Dataframe with methods to extract certain column features
         from CHSI datasets.  Also contain self loading and preprocessing
         methods.
+        Parameters
+        ----------
+        filename : csv filename that contains CHSI data
         """
-        self._data = self.load_data(filename)
+        self.df = self.load_data(filename)
         self.filename = filename
 
     @classmethod
@@ -20,15 +22,28 @@ class Dataset(pd.DataFrame):
         return pd.read_csv(filename)
 
     def preproc(self):
-        # dropping confidence interval columns. Confidence interval columns
-        # starting with 'CI_'
+        """
+        Preprocessing CHSI dataset.  First, dropping the confidence interval
+        columns that starts with 'CI_'.  Then changing some invalid values to
+        np.nan according to DEFINEDDATAVALUE.csv. Also constructs the five digit
+        FIPS code from State_FIPS_Code and County_FIPS_Code.
+        """
         cols = self._data.columns.values
         cols_drop = [c for c in cols if 'CI_' in c]
-        self._data = self._data.drop(self._data[cols_drop], axis=1)
+        self.df = self.df.drop(self.df[cols_drop], axis=1)
 
-        self._data = self._data.replace([-1111, -2222], [np.nan, np.nan])
+        self.df = self.df.replace([-1,-1111,-1111.1,-2,-2222.2,-2222,-9999,-9989.9],
+                      [np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan])
+
+        self.df['FIPS'] = self.df['State_FIPS_Code'].apply(lambda x:str(x).zfill(2)) +
+                          self.df['County_FIPS_Code'].apply(lambda x:str(x).zfill(3))
         return None
 
-    def extract(self, age, race, cod):
-        feature_col = str(age)
-        return
+    def lookup(self, age, race, cod) -> pd.DataFrame:
+        """
+        Takes in age, race, cod as argument and returns a dataframe slice from
+        the CHSI dataset with the FIPS column and feature column.
+        """
+        feature_col = str(age)+'_'+str(race)+'_'+str_cod
+        print(feature_col)
+        return self.df[['FIPS', feature_col]]
